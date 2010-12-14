39d594a0d7f0744e9be579050b876bec12d3a79d
Konrad Malawski <konrad.malawski@project13.pl>
2010-12-14 14:53:38 +0100

    sąsiedztwo lewo stronne tylko

--- src/pl/project/ca/Board.java
+++ src/pl/project/ca/Board.java
@@ -62,9 +62,9 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
     for (int x = 0; x < points.length; ++x) {
       for (int y = 0; y < points[x].length; ++y) {
         //TODO: initialize the neighborhood of points[x][y] cell
-        tryToAdd(points[x][y],x-1,y+1); tryToAdd(points[x][y],x, y+1); tryToAdd(points[x][y],x+1,y+1);
+        tryToAdd(points[x][y],x-1,y+1); //tryToAdd(points[x][y],x, y+1); tryToAdd(points[x][y],x+1,y+1);
         tryToAdd(points[x][y],x-1,y); /*                            */ tryToAdd(points[x][y], x+1, y);
-        tryToAdd(points[x][y],x-1,y-1); tryToAdd(points[x][y],x, y-1); tryToAdd(points[x][y], x+1, y-1);
+        tryToAdd(points[x][y],x-1,y-1); //tryToAdd(points[x][y],x, y-1); tryToAdd(points[x][y], x+1, y-1);
       }
     }
   }
