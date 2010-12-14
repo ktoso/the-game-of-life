cd1b11d42d26d718d85cb5a7f07bc976f9982321
Konrad Malawski <konrad.malawski@project13.pl>
2010-12-14 04:30:11 +0100

    done, glider working

--- src/pl/project/ca/Board.java
+++ src/pl/project/ca/Board.java
@@ -133,24 +133,26 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
         if (points[x][y].getState() != 0) {
           //TODO: set the proper color of the cell
           switch (points[x][y].getState()) {
+            case 0:
+              g.setColor(new Color(0xffffff));
             case 1:
-              g.setColor(new Color(0x0000ff));
-              break;
-            case 2:
-              g.setColor(new Color(0x00ff00));
-              break;
-            case 3:
-              g.setColor(new Color(0xff0000));
-              break;
-            case 4:
               g.setColor(new Color(0x000000));
               break;
-            case 5:
-              g.setColor(new Color(0x444444));
-              break;
-            case 6:
-              g.setColor(new Color(0xffffff));
-              break;
+//            case 2:
+//              g.setColor(new Color(0x00ff00));
+//              break;
+//            case 3:
+//              g.setColor(new Color(0xff0000));
+//              break;
+//            case 4:
+//              g.setColor(new Color(0x000000));
+//              break;
+//            case 5:
+//              g.setColor(new Color(0x444444));
+//              break;
+//            case 6:
+//              g.setColor(new Color(0xffffff));
+//              break;
           }
           g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
         }
--- src/pl/project/ca/Point.java
+++ src/pl/project/ca/Point.java
@@ -6,7 +6,13 @@ public class Point {
   private ArrayList<Point> neighbors;
   private int              currentState;
   private int              nextState;
-  private int              numStates = 6;
+  private int numStates = 2;
+
+  private final int ALIVE = 1;
+  private final int DEAD  = 0;
+
+  private final int MIN_FRIENDS = 2;
+  private final int MAX_FRIENDS = 3;
 
   public Point() {
     currentState = 0;
@@ -28,8 +34,10 @@ public class Point {
 
   public void calculateNewState() {
     //TODO: insert logic which updates according to currentState and number of active neighbors
-    if (shouldThisDie()) {
-      changeState();
+    if (getState() == ALIVE) {
+      nextState = shouldThisDie() ? DEAD : ALIVE;
+    } else {//(getState() == DEAD){
+      nextState = shouldThisBeBorn() ? ALIVE : DEAD;
     }
   }
 
@@ -43,8 +51,31 @@ public class Point {
 
   //TODO: write method counting all active neighbors of THIS point
   private boolean shouldThisDie() {
-    System.out.println(neighbors.size());
-    int neighborsCount = neighbors.size();
-    return neighborsCount > 3 || neighborsCount < 2;
+    int aliveFriends = countAliveFriends();
+
+    boolean shouldDie = aliveFriends > MAX_FRIENDS || aliveFriends < MIN_FRIENDS;
+    System.out.println("alive friends: " + aliveFriends + "; " + (shouldDie ? "dying" : "living"));
+
+    return shouldDie;
+  }
+
+  private boolean shouldThisBeBorn() {
+    int aliveFriends = countAliveFriends();
+    boolean shouldStart = aliveFriends == MAX_FRIENDS;
+    System.out.println("alive friends: " + aliveFriends + "; " + (shouldStart ? "starting" : "still dead"));
+
+    return shouldStart;
+  }
+
+  private int countAliveFriends() {
+    int aliveFriends = 0;
+
+    System.out.print("friends: " + neighbors.size() + "; ");
+    for (Point neighbor : neighbors) {
+      if (neighbor.getState() == 1) {
+        ++aliveFriends;
+      }
+    }
+    return aliveFriends;
   }
 }
